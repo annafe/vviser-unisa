d62bb26d3e57b0e3e53c6440594f592af8a042c2
micheleroviello@gmail.com <micheleroviello@gmail.com@e0164577-9df5-3ae5-6c90-3f899a537ada>
2013-12-14 17:38:15 +0000

    Metodi di ricerca

--- trunk/vviser/src/it/unisa/vviser/storage/DBAccount.java
+++ trunk/vviser/src/it/unisa/vviser/storage/DBAccount.java
@@ -4,7 +4,11 @@ import it.unisa.vviser.entity.Account;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 /**
  * 
@@ -12,6 +16,10 @@ import java.sql.SQLException;
  *
  */
 public class DBAccount {
+	
+	private Connection conn;
+	private String query;
+	private PreparedStatement st;
 	
 	/**
 	 * Costruttore vuoto della classe DBAccount
@@ -28,9 +36,6 @@ public class DBAccount {
 	 * @throws SQLException
 	 */
 	public boolean addAccount(Account a) throws SQLException{
-		Connection conn = null;
-		PreparedStatement st = null;
-		String query;
 		
 		try{
 			conn = DBConnectionPool.getConnection();
@@ -72,6 +77,8 @@ public class DBAccount {
 		return false;
 	}
 	
+	
+	// METODI DI ELIMINAZIONE
 	/**
 	 * Elimina un account dal sistema
 	 * @param a Account da eliminare
@@ -79,9 +86,6 @@ public class DBAccount {
 	 * @throws SQLException
 	 */
 	public boolean removeAccount(Account a) throws SQLException{
-		Connection conn = null;
-		PreparedStatement st = null;
-		String query;
 		
 		try{
 			conn = DBConnectionPool.getConnection();
@@ -101,4 +105,145 @@ public class DBAccount {
 		}
 		return false;
 	}
+	
+	// METODI DI RICERCA
+	
+	/**
+	 * Ricerca account in base al nome
+	 * @param name Nome associato all'account
+	 * @return Lista degli account trovati
+	 * @throws SQLException 
+	 */
+	public List<Account> findByName(String name) throws SQLException{
+		
+		
+		try{
+			conn = DBConnectionPool.getConnection();
+			
+			query = "SELECT * FROM "+DBNames.TABLE_UTENTE
+					+" WHERE "+DBNames.ATTR_UTENTE_NOME+"="+name;
+			st = conn.prepareStatement(query);
+			ResultSet rs = st.executeQuery();
+			
+			return fillResults(rs);
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			st.close();
+			DBConnectionPool.releaseConnection(conn);
+		}
+		return null;
+		
+	}
+	
+	/**
+	 * Ricerca account in base al cognome
+	 * @param surname Nome associato all'account
+	 * @return Lista degli account trovati
+	 * @throws SQLException 
+	 */
+	public List<Account> findBySurname(String surname) throws SQLException{
+		
+		try{
+			conn = DBConnectionPool.getConnection();
+			
+			query = "SELECT * FROM "+DBNames.TABLE_UTENTE
+					+" WHERE "+DBNames.ATTR_UTENTE_COGNOME+"="+surname;
+			st = conn.prepareStatement(query);
+			ResultSet rs = st.executeQuery();
+			
+			return fillResults(rs);
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			st.close();
+			DBConnectionPool.releaseConnection(conn);
+		}
+		return null;
+		
+	}
+	 /**
+	  * Ricerca account in base al nome o al cognome
+	  * @param name Nome associato all'account
+	  * @param surname Cognome associato all'account
+	  * @return Lista degli account trovati
+	  * @throws SQLException
+	  */
+	public List<Account> findByNameSurname(String name, String surname) throws SQLException{
+		
+		try{
+			conn = DBConnectionPool.getConnection();
+			
+			query = "SELECT * FROM "+DBNames.TABLE_UTENTE
+					+" WHERE "+DBNames.ATTR_UTENTE_COGNOME+"="+surname
+					+" OR "+DBNames.ATTR_UTENTE_NOME+"="+name;
+			st = conn.prepareStatement(query);
+			ResultSet rs = st.executeQuery();
+			
+			return fillResults(rs);
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			st.close();
+			DBConnectionPool.releaseConnection(conn);
+		}
+		return null;
+		
+	}
+
+	// METODI DI SUPPORTO PER LA CLASSE
+
+	/**
+	 * Riempie una lista di Account con i risultati di una ricerca
+	 * @param rs ResultSet con i risultati di una query
+	 * @return Lista di Account trovati
+	 * @throws SQLException
+	 */
+	private List<Account> fillResults(ResultSet rs) throws SQLException {
+		List<Account> results = new ArrayList<Account>();
+		
+		while(rs.next()){
+			int id = rs.getInt("id");
+			String n = rs.getString(DBNames.ATTR_UTENTE_NOME);
+			String c = rs.getString(DBNames.ATTR_UTENTE_COGNOME);
+			GregorianCalendar dn = creaData(rs.getString(DBNames.ATTR_UTENTE_DATADINASCITA));
+			String cn = rs.getString(DBNames.ATTR_UTENTE_COMUNEDINASCITA);
+			String pn = rs.getString(DBNames.ATTR_UTENTE_PROVINCIADINASCITA);
+			String cf = rs.getString(DBNames.ATTR_UTENTE_CODICEFISCALE);
+			String p = rs.getString(DBNames.ATTR_UTENTE_PASSWORD);
+			String e = rs.getString(DBNames.ATTR_UTENTE_EMAIL);
+			String d = rs.getString(DBNames.ATTR_UTENTE_DIPARTIMENTO_NOME);
+			String t = rs.getString(DBNames.ATTR_UTENTE_TIPOLOGIA);
+			
+			results.add(new Account(id, n, c, dn, cn, pn, cf, p, e, d, t)); 
+		}
+		
+		return results;
+	}
+	
+	
+	
+	/**
+	 * crea un GregorianCalendar data una stringa in formato "YYYY/MM/DD"
+	 * @param data stringa che contiene la data in formato "YYYY/MM/DD"
+	 * @return un GregorianCalendar contenente la data inserita in input
+	 */
+	private GregorianCalendar creaData(String data){
+		GregorianCalendar toReturn;
+		String[] tokens = new String[3];
+		int[] g=new int[3];
+			
+		tokens = data.split("/");
+			
+		g[0]=Integer.parseInt(tokens[0]); 
+		g[1]=Integer.parseInt(tokens[1]); 
+		g[2]=Integer.parseInt(tokens[2]);
+			
+		toReturn = new GregorianCalendar(g[0],g[1],g[2]);
+		return toReturn;
+	}
+	
 }
