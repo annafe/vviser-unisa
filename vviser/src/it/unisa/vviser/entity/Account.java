6fc6ecaa3db9288b31061ff6cf57751acbeb4c05
micheleroviello@gmail.com <micheleroviello@gmail.com@e0164577-9df5-3ae5-6c90-3f899a537ada>
2013-12-14 11:13:37 +0000

    Aggiunta costruttori, documentazione di costruttori e metodi Eliminazione dell'attributo userName e ralativi metodi Sostituzione del tipo dell'attributo dataDiNascita da Date (deprecato) a GregorianCalendar

--- vviser/src/it/unisa/vviser/entity/Account.java
+++ vviser/src/it/unisa/vviser/entity/Account.java
@@ -1,121 +1,234 @@
 package it.unisa.vviser.entity;
 
 import java.util.Date;
-
-
+import java.util.GregorianCalendar;
 
 
 /**
-DOCUMENTAZIONE:
-- COSA RAPPRESENTA LA CLASSE
-- A COSA SERVE
-- COME SI USA
-@author Eugenio Gigante
-@version 1.0
-*/
-
+ * 
+ * @author Eugenio Gigante, Michele Roviello
+ *
+ */
 public class Account {
-	private String id;	
+	
+	private int id;	
 	private String nome;
-private String cognome;
-private Date dataDiNascita;
-private String comuneDiNascita;
-private String provinciaDiNascita;
-private String userName;
-private String codiceFiscale;
-private String password;
-private String email;
-private String dipartimento_Nome;
-private String tipologia;
+	private String cognome;
+	private GregorianCalendar dataDiNascita;
+	private String comuneDiNascita;
+	private String provinciaDiNascita;
+	private String codiceFiscale;
+	private String password;
+	private String email;
+	private String dipartimento;
+	private String tipologia;
 
-
-/**
-DOCUMENTAZIONE:
-- COSA FA IL METODO
-- COME USARLO
-@param x COSA RAPPRESENTA x?
-@return COSA RESTITUISCE
-@throws Ex QUANDO VIENE LANCIATA?
-*/
-public void setNome (String nome){
-	this.nome=nome;
-}
-public String getNome(){
-	return this.nome;
-}
-
-public void setCognome (String cognome){
-	this.cognome=cognome;
-}
-public String getCognome(){
-	return this.cognome;
-}
-
-public void setDataDiNascita (Date date){
-	this.dataDiNascita=date;
-}
-public Date getDataDiNascita(){
-	return this.dataDiNascita;
-}
-
-public void setComuneDiNascita (String comuneDiNascita){
-	this.comuneDiNascita=comuneDiNascita;
-}
-public String getComuneDiNascita(){
-	return this.comuneDiNascita;
-}
-
-public void setProvinciaDiNascita (String provinciaDiNascita){
-	this.provinciaDiNascita=provinciaDiNascita;
-}
-public String getProvinciaDiNascita(){
-	return this.provinciaDiNascita;
-}
-
-public void setUserName (String userName){
-	this.userName=userName;
-}
-public String getUserName(){
-	return this.userName;
-}
-
-public void setCodiceFiscale (String codiceFiscale){
-	this.codiceFiscale=codiceFiscale;
-}
-public String getCodiceFiscale(){
-	return this.codiceFiscale;
-}
-
-public void setPassword (String password){
-	this.password=password;
-}
-public String getPassword(){
-	return this.password;
-}
-
-
-public void setEmail (String email){
-	this.email=email;
-}
-public String getemail(){
-	return this.email;
-}
-public void setDipartimento_Nome (String dipartimento_Nome ){
-	this.dipartimento_Nome =dipartimento_Nome ;
-}
-public String getDipartimento_Nome (){
-	return this.dipartimento_Nome ;
-}
-public void setTipologia (String tipologia ){
-	this.tipologia  =tipologia  ;
-}
-public String getTipologia  (){
-	return this.tipologia  ;
-}
-public void setID (String id ){
-	this.id  =id  ;
-}
-public String getID  (){
-	return this.id  ;
-}
+	/**
+	 * Il costruttore vuoto della classe Account
+	 */
+	public Account(){ }
+	
+	public Account(int id, String nome, String cognome, 
+			GregorianCalendar dataDiNascita, String comuneDiNascita, 
+			String provinciaDiNascita, String codiceFiscale, 
+			String password, String email, String dipartimento, String tipologia){
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.dataDiNascita = dataDiNascita;
+		this.comuneDiNascita = comuneDiNascita;
+		this.provinciaDiNascita = provinciaDiNascita;
+		this.codiceFiscale = codiceFiscale;
+		this.password = password;
+		this.email = email;
+		this.dipartimento = dipartimento;
+		this.tipologia = tipologia;
+	}
+	
+	/**
+	 * Imposta il nome associato all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param nome Nome da memorizzare
+	 */
+	public void setNome (String nome){
+		this.nome=nome;
+	}
+	
+	/**
+	 * Restituisce il nome associato all'account
+	 * @return Nome associato all'account
+	 */
+	public String getNome(){
+		return this.nome;
+	}
+	
+	/**
+	 * Imposta il cognome associato all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param cognome Cognome da memorizzare
+	 */
+	public void setCognome (String cognome){
+		this.cognome=cognome;
+	}
+	
+	/**
+	 * Restituisce il cognome associato all'account
+	 * @return Cognome associato all'account
+	 */
+	public String getCognome(){
+		return this.cognome;
+	}
+	
+	/**
+	 * Imposta la data di nascita associata all'account con la data specificata 
+	 * sostituendo il valore precedente
+	 * @param date Data di nascita da memorizzare
+	 */
+	public void setDataDiNascita (GregorianCalendar date){
+		this.dataDiNascita=date;
+	}
+	
+	/**
+	 * Restituisce la data di nascita associata all'account
+	 * @return Data di nascita
+	 */
+	public GregorianCalendar getDataDiNascita(){
+		return this.dataDiNascita;
+	}
+	
+	/**
+	 * Imposta il comune di nascita associato all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param comuneDiNascita Comune di nascita da memorizzare
+	 */
+	public void setComuneDiNascita (String comuneDiNascita){
+		this.comuneDiNascita=comuneDiNascita;
+	}
+	
+	/**
+	 * Restituisce il comune di nascita associato all'account
+	 * @return Comune di nascita
+	 */
+	public String getComuneDiNascita(){
+		return this.comuneDiNascita;
+	}
+	
+	/**
+	 * Imposta la provincia di nascita associata all'account col valore specificato 
+	 * sostituendo il valore precedente
+	 * @param provinciaDiNascita Provincia di nascita da memorizzare
+	 */
+	public void setProvinciaDiNascita (String provinciaDiNascita){
+		this.provinciaDiNascita=provinciaDiNascita;
+	}
+	
+	/**
+	 * Restituisce la provincia di nascita associata all'account
+	 * @return Provincia di nascita
+	 */
+	public String getProvinciaDiNascita(){
+		return this.provinciaDiNascita;
+	}
+	
+	/**
+	 * Imposta il codice fiscale associato all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param codiceFiscale Codice fiscale da memorizzare
+	 */
+	public void setCodiceFiscale (String codiceFiscale){
+		this.codiceFiscale=codiceFiscale;
+	}
+	
+	/**
+	 * Restituisce il codice fiscale associato all'account
+	 * @return Codice fiscale
+	 */
+	public String getCodiceFiscale(){
+		return this.codiceFiscale;
+	}
+	
+	/**
+	 * Imposta la password  associata all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param password Password da memorizzare
+	 */
+	public void setPassword (String password){
+		this.password=password;
+	}
+	
+	/**
+	 * Restituisce la password associata all'account
+	 * @return Password
+	 */
+	public String getPassword(){
+		return this.password;
+	}
+	
+	/**
+	 * Imposta l'indirizzo email asscociato all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param email Indirizzo Email da memorizzare
+	 */
+	public void setEmail (String email){
+		this.email=email;
+	}
+	
+	/**
+	 * Restituisce l'indirizzo email associato all'account
+	 * @return Indirizzo email
+	 */
+	public String getEmail(){
+		return this.email;
+	}
+	
+	/**
+	 * Imposta il nome del dipartimento associato all'account col valore specificato
+	 * sostituendo il valore precedente
+	 * @param dipartimento_Nome Nome del dipartimento
+	 */
+	public void setDipartimento_Nome (String dipartimento){
+		this.dipartimento =dipartimento;
+	}
+	
+	/**
+	 * Restituisce il nome del dipartimento associato all'account
+	 * @return Nome del dipartimento
+	 */
+	public String getDipartimento_Nome (){
+		return this.dipartimento;
+	}
+	
+	/**
+	 * Imposta la tipologia di appartenenza associata all'account 
+	 * sostituendo il valore precedente
+	 * @param tipologia Tipologia da memorizzare
+	 */
+	public void setTipologia (String tipologia ){
+		this.tipologia  =tipologia  ;
+	}
+	
+	/**
+	 * Restituisce la tipologia associata all'account
+	 * @return Tipolodia di account
+	 */
+	public String getTipologia  (){
+		return this.tipologia  ;
+	}
+	
+	/**
+	 * Imposta il codice ID associato all'account 
+	 * sostituendo il valore precedente
+	 * @param id Codice ID da memorizzare
+	 */
+	public void setID (int id ){
+		this.id = id  ;
+	}
+	
+	/**
+	 * Restituisce il codice ID associato all'account
+	 * @return Codice ID
+	 */
+	public int getID  (){
+		return this.id  ;
+	}
 }
